438a9cf03b07d2059f7026a0d99b99018ef47f24
9ganzi <gutiday14@gmail.com>
2021-12-15 05:03:57 -0500

    attempt for exchanging s_ij
    this is more like a pseudo code, still struggling to review c++ syntax

--- Party.cpp
+++ Party.cpp
@@ -3,25 +3,29 @@
 
 vector<member> party::all_members = vector<member>();
 
-party::party() {};
+party::party(){};
 
-party::party(unsigned int size_of_party, unsigned int threshold, vector<member> all_members){ 
+party::party(unsigned int size_of_party, unsigned int threshold, vector<member> all_members)
+{
     this->size_of_party = size_of_party;
     this->threshold = threshold;
-    set_party_members(all_members);//to update the all_members vector
-    for(int i = 0; i < all_members.size(); i++){
+    set_party_members(all_members); //to update the all_members vector
+    for (int i = 0; i < all_members.size(); i++)
+    {
         this->all_members[i].size_of_party = this->size_of_party; //sets the size_of_party variable of the member
-        this->all_members[i].threshold = this->threshold; //sets the threshold variable of the member
-        
+        this->all_members[i].threshold = this->threshold;         //sets the threshold variable of the member
+        // 12/15 Jae
+        // this->all_members[i].setIndex(i)                          // set index of the member
     }
 };
 
 //copy constructor
-party::party(const party& p){
+party::party(const party &p)
+{
     this->size_of_party = p.size_of_party;
     this->threshold = p.threshold;
-    for(int i = 0; i < this->size_of_party; i++){
+    for (int i = 0; i < this->size_of_party; i++)
+    {
         this->all_members[i] = p.all_members[i];
-
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //Group 2
-//Fall 2021 
-//CSc 480 
+//Fall 2021
+//CSc 480
 //Distributed Key Generation
 
 #include <NTL/ZZ_pX.h>
@@ -27,61 +27,71 @@ party all_nodes;
 
 /* END GLOBAL VARIABLES */
 
-int main(int argc, char* argv[]) {
-	// Initialize n (total members playing) and t (threshold)
+int main(int argc, char *argv[])
+{
+    // Initialize n (total members playing) and t (threshold)
     int n, t;
     // optional args n = $1, t = $2
-    if (argc == 2) {
+    if (argc == 2)
+    {
         n = stoi(argv[1]);
         t = n / 2; // threshold is default half of total members
-    } else if (argc == 3) {
+    }
+    else if (argc == 3)
+    {
         n = stoi(argv[1]);
         t = stoi(argv[2]);
-    } else {
+    }
+    else
+    {
         n = 14;
         t = 7;
     }
-    
-	// Read and/or initialize p, q, and g and set ZZ_p to q
-	readParams();
-	initNTLRandom();
-	
-	vector<member> members_in_party_one = { };
+
+    // Read and/or initialize p, q, and g and set ZZ_p to q
+    readParams();
+    initNTLRandom();
+
+    vector<member> members_in_party_one = {};
     unsigned int size_of_party = 10;
     unsigned int size_of_threshold = 1;
 
-    //uses the empty constructor to declare this object, will need to manually update the size_of_party, size_of_threshold, and all_members vector 
-    
-	party party1 = party(); 
-    party1.set_party_size(size_of_party); //sets the size_of_party
-    party1.set_threshold_size(size_of_threshold);//sets the size_of_threshold
+    //uses the empty constructor to declare this object, will need to manually update the size_of_party, size_of_threshold, and all_members vector
+
+    party party1 = party();
+    party1.set_party_size(size_of_party);           //sets the size_of_party
+    party1.set_threshold_size(size_of_threshold);   //sets the size_of_threshold
     party1.set_members_and_party_threshold_sizes(); //creates the new members and updates those members size_of_party and size_of_threshold variables
-	
-	
-	//Alternatively, one can uses the constructor that takes the 3 parameter below to declare this object and these variables will updated to both the party and members
-    //party party1 = party(size_of_party, size_of_threshold, members_in_party_one);  
+
+    //Alternatively, one can uses the constructor that takes the 3 parameter below to declare this object and these variables will updated to both the party and members
+    //party party1 = party(size_of_party, size_of_threshold, members_in_party_one);
 
     ZZ_pX function;
-           
+
     //generates random little polynomials for every member in party 1. I print f which will show all coefficients in the polynomial
-    for(int i = 0; i < party1.size_of_party; i++){
+    for (int i = 0; i < party1.size_of_party; i++)
+    {
         function = random_ZZ_pX(party1.size_of_party);
         party1.all_members[i].set_little_function(function);
-        cout << "little function for member, " << party1.all_members[i].getIndex()  << ", is: \n" <<  party1.all_members[i].get_little_function()  << endl; 
+        cout << "little function for member, " << party1.all_members[i].getIndex() << ", is: \n"
+             << party1.all_members[i].get_little_function() << endl;
     }
-	
-	//brings 3 coefficients from the little polynomial of the member with index 10 who is in party1.
-    cout << "\nlittle function coefficient at index 0 for member " << party1.all_members[9].getIndex()  << ", is: \n" <<  party1.all_members[9].get_little_function()[0]  << endl; 
-    cout << "\nlittle function coefficient at index 1 for member " << party1.all_members[9].getIndex()  << ", is: \n" <<  party1.all_members[9].get_little_function()[1]  << endl; 
-    cout << "\nlittle function coefficient at index 2 for member " << party1.all_members[9].getIndex()  << ", is: \n" <<  party1.all_members[9].get_little_function()[2]  << endl; 
+
+    //brings 3 coefficients from the little polynomial of the member with index 10 who is in party1.
+    cout << "\nlittle function coefficient at index 0 for member " << party1.all_members[9].getIndex() << ", is: \n"
+         << party1.all_members[9].get_little_function()[0] << endl;
+    cout << "\nlittle function coefficient at index 1 for member " << party1.all_members[9].getIndex() << ", is: \n"
+         << party1.all_members[9].get_little_function()[1] << endl;
+    cout << "\nlittle function coefficient at index 2 for member " << party1.all_members[9].getIndex() << ", is: \n"
+         << party1.all_members[9].get_little_function()[2] << endl;
 
     //generate_params();
-	
-	//cout << "Create party of " << n << "players with threshold " << t << "." << endl;
+
+    //cout << "Create party of " << n << "players with threshold " << t << "." << endl;
     //Party party1(n, t);
     //Member m(3, t);
     //cout << "member number " << m.getIndex() << endl;
     //cout << "party size of threshold " << party1.threshold << endl;
 
-    return 0; 
+    return 0;
 }
